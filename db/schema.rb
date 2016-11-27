83437b897aade3b31f4eff1a93b9f610d9a4f49b
remotezen <remotezen@bell.net>
2016-11-26 21:52:06 -0500

    santity check

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126143030) do
+ActiveRecord::Schema.define(version: 20161126164000) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invitations", force: :cascade do |t|
     t.string "email"
